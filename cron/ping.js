f028107266b889ed763dc2135f023a909adfbba7
AddyAddline <21ucc013@lnmiit.ac.in>
2024-07-04 23:07:03 +0530

    chore: Update ping cron job to use axios for server pinging

--- cron/ping.js
+++ cron/ping.js
@@ -1,17 +1,17 @@
-const cron = require('node-cron');
-require('dotenv').config();
+const cron = require("node-cron");
+const axios = require("axios");
+require("dotenv").config();
 
 const { NODE_ENV, PORT = 3000, BASE_URL_PROD } = process.env;
 const BASE_URL =
-  NODE_ENV === 'development' ? 'http://localhost' : BASE_URL_PROD;
-const FULL_URL = NODE_ENV === 'development' ? `${BASE_URL}:${PORT}` : BASE_URL;
+  NODE_ENV === "development" ? "http://localhost" : BASE_URL_PROD;
+const FULL_URL = NODE_ENV === "development" ? `${BASE_URL}:${PORT}` : BASE_URL;
 
-cron.schedule('*/14 * * * *', async () => {
+cron.schedule("*/14 * * * *", async () => {
   try {
-    const response = await fetch(`${FULL_URL}/ping`);
-    const data = await response.json();
-    console.log('Server responded to /ping:', data);
+    const response = await axios.get(`${FULL_URL}/ping`);
+    console.log("Server responded to /ping:", response.data);
   } catch (error) {
-    console.error('Error pinging the server:', error);
+    console.error("Error pinging the server:", error.message);
   }
 });
